a3d74a83647bc03a786426e4aa8463adf3b47f7f
kirargomedo23 <kirargomedo@gmail.com>
2022-10-09 09:55:36 -0500

    estructura

---
+++ src/app/layouts/family/family.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FamilyRoutingModule } from './family-routing.module';
+import { FamilyComponent } from './family.component';
+
+
+@NgModule({
+  declarations: [
+    FamilyComponent
+  ],
+  imports: [
+    CommonModule,
+    FamilyRoutingModule
+  ]
+})
+export class FamilyModule { }
